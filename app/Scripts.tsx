b358338387663f305d0348d513602ab39c777cbd
NikitaIT <nikita2008101@gmail.com>
2026-01-05 01:30:53 +0100

    feat(hero): add ga

--- app/Scripts.tsx
+++ app/Scripts.tsx
@@ -2,13 +2,28 @@ import Script from "next/script";
 
 export function Scripts() {
   return (
-    <Script id="microsoft-clarity" strategy="afterInteractive">
-      {/* microsoft clarity */}
-      {`(function(c,l,a,r,i,t,y){
+    <>
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {/* microsoft clarity */}
+        {`(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "uwfi10hryc");`}
-    </Script>
+      </Script>
+
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-SMHY9BMCRK"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SMHY9BMCRK');`}
+      </Script>
+    </>
   );
 }
